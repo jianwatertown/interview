5bbea77696d10ad6cbaad79f3c1c161eb3e4eb75
Jian Wang <jian.wang>
2016-07-25 23:11:02 -0700

    robot grid int

--- interviewPractice/src/careercup/dynamicProgramming/RobotInGrid.java
+++ interviewPractice/src/careercup/dynamicProgramming/RobotInGrid.java
@@ -1,20 +1,103 @@
 package careercup.dynamicProgramming;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
 
 public class RobotInGrid {
 	
+/**
+ * 
+ * 	Key 1: recursive structure, the LinkedList<Point> as return type
+ * 
+ * 	Key 2: use a visited set
+ * 
+ *  
+ * @param r
+ * @param c
+ * @return
+ */
 	
-	public LinkedList<Integer> findPath(){
-		return null;
+	public LinkedList<Point> findPath(int r, int c){
+		return findPath(r,c,0,0, new HashSet<Point>());
 	} 
 	
-	public LinkedList<Integer> findPath(int x, int y, ){
+
+	// x in [0,c-1], y in [0,r-1]
+	public LinkedList<Point> findPath(int r, int c, int x, int y, Set<Point> visited){
 		
+		Point p = new Point(x,y);
+		
+		// 0. found the end
+		if((x==c-1)&&(y==r-1)){
+			LinkedList<Point> path = new LinkedList<Point>();
+			path.add(p);
+			return path;
+		}
+		
+		// 1. visited? then somebody else must have taken care of this
+		if(visited.contains(p)){
+			return null;
+		}		
+		
+		// 2. exceeds
+		if((x>c-1)||(y>r-1)||(isOffLimit(x,y))){
+			visited.add(p);
+			return null;
+		}
+		
+		// 3. looking for a path
+		else{
+			visited.add(p);
+			LinkedList<Point> pathRight = findPath(r,c,x+1,y,visited);
+			
+			if(pathRight!=null){
+				pathRight.add(p);
+				return pathRight;
+			}
+			else{
+				LinkedList<Point> pathDown = findPath(r,c,x,y+1,visited);
+				if(pathDown!=null){
+					pathDown.add(p);
+					return pathDown;
+				}
+				else{
+					return null;
+				}
+			}
+		}
 	}
 	
 	
 	public boolean isOffLimit(int x, int y){
-		return true;
+		if(x==1 &&y==0){
+			return true;
+		}
+		return false;
+	}
+	
+	public class Point{
+		int x;
+		int y;
+		
+		public Point(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		
+		@Override
+		public String toString(){
+			return "x = "+x+" y = "+y;
+		}
+	}
+	
+	public static void main(String[] args){
+		RobotInGrid robot = new RobotInGrid();
+		LinkedList<Point> path = robot.findPath(3, 3);
+		for(Point p:path){
+			System.out.println(p);
+		}
 	}
 }
