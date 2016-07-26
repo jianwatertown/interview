d3b84a25c99e98cb3df8c4e2f8578474638142bb
Jian Wang <jian.wang>
2016-07-26 14:33:32 -0700

    magic index

---
+++ interviewPractice/src/careercup/dynamicProgramming/MagicIndex.java
@@ -0,0 +1,45 @@
+package careercup.dynamicProgramming;
+
+public class MagicIndex {
+
+/**
+ * Tricky when the numbers are not unique	
+ * 
+ * Great base case break down
+ * 
+ * @param input
+ */
+	
+	public void findMagicIndex(int[] input){
+		findMagicIndex(input,0,input.length-1);
+	}
+	
+	public void findMagicIndex(int[] input, int b, int e){
+
+		// 1. end condition
+		if(b>e){
+			return;
+		}
+		// 2. b<=e
+		else{
+			int index = (b+e)/2;
+			if(input[index]==index){
+				System.out.println(index);
+			}
+			
+			// search left and right
+			int leftIndex = Math.min(index-1, input[index]);
+			int rightIndex = Math.max(index+1, input[index]);
+			
+			findMagicIndex(input,b,leftIndex);
+			findMagicIndex(input,rightIndex,e);
+		}
+	}
+	
+	public static void main(String[] args){
+		MagicIndex instance = new MagicIndex();
+		int[] input = {1,2,3,4,5,5};
+		instance.findMagicIndex(input);
+	}
+	
+}
