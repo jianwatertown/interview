bd71ff3643e9cba697ea29aa0dcc1c5c06d0b3b8
Jian Wang <jian.wang>
2016-08-13 22:13:48 -0700

    added diagrams

--- elevatorProject/src/parking/valet/ValetService.java
+++ elevatorProject/src/parking/valet/ValetService.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
