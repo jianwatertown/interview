f7128b6f5f231dceaa9b2ae84ed530239b0ddb68
Jian Wang <jian.wang>
2016-10-18 09:22:40 -0700

    kicked 3sum's ass

--- interviewPractice/src/dynamicProgramming/ThreeSum.java
+++ interviewPractice/src/dynamicProgramming/ThreeSum.java
@@ -22,73 +22,47 @@ import java.util.List;
  */
 public class ThreeSum {
 	
-    public List<List<Integer>> threeSum(int[] nums) {
-    	
-    	List<List<Integer>> result = new ArrayList<List<Integer>>();
-    	
-    	if(nums==null||nums.length<3) {return result;}
-    	
-    	// 1. O(NLogN) sort
-    	Arrays.sort(nums);
-    	
-    	// 2. O(N) find the first element<=0
-    	int middle = indexOfFirstNoBiggerThanKey(nums,0);
-
-    	if(middle==-1 || 				// nothing smaller than 0
-    			middle>=nums.length-1){	// fewer than 1 bigger than 0 
-    		return result;
-    	}
-    	
-    	// 3. from middle and expand left and right
-    	int left = middle -1;
-    	int right = middle +1;
-    	while(left>=0 && right<=nums.length-1){
-    		// right on 
-    		if(nums[left]+nums[middle]+nums[right]==0){
-    			List<Integer> oneResult = new ArrayList<Integer>();
-    			oneResult.add(nums[left]);
-    			oneResult.add(nums[middle]);
-    			oneResult.add(nums[right]);
-    			result.add(oneResult);
-    			left --;
-    			right ++;
-    		}
-    		else if(nums[left]+nums[middle]+nums[right]>0){
-    			left --;
-    		}
-    		else{	// nums[left]+nums[middle]+nums[right]<0
-    			right ++;
-    		}
-    		// skip the same thing
-    		while(left>0 && nums[left-1]==nums[left]){
-    			left--;
-    		}
-    		while(right<nums.length-1 && nums[right+1]==nums[right]){
-    			right++;
-    		}
-    	}
-    	return result;
-    }
-    
-    public static int indexOfFirstNoBiggerThanKey(int[] a, int key) {
-        int lo = 0;
-        int hi = a.length - 1;
-        int mid = lo + (hi - lo) / 2;
-        while (lo <= hi) {
-            // Key is in a[lo..hi] or not present.
-            mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
-            else return mid;			// exact match
-        }
-        // now a[mid] is just smaller than or bigger than key
-        if(a[mid]<key) {
-        	return mid;
-        }
-        else{
-        	return mid-1;
-        }
-    }
+	   public List<List<Integer>> threeSum(int[] nums) {
+	    	
+	    	List<List<Integer>> result = new ArrayList<List<Integer>>();
+	    	
+	    	if(nums==null||nums.length<3) {return result;}
+	    	
+	    	// O(NLogN) sort
+	    	Arrays.sort(nums);
+	    	
+	    	// from 1st to end-2
+	    	for(int i=0;i<nums.length-2;i++){
+	    		// 1. skip current if it is the same
+	    		if(i>0 && nums[i-1]==nums[i]) continue;
+	    		
+	    		// 2. left starts from next, right starts from end
+	    		int left = i+1;
+	    		int right = nums.length-1;
+	   
+	    		while(left<right){
+	    			int sum = nums[i]+nums[left]+nums[right];
+	    			if(sum==0) {
+						ArrayList<Integer> tmp = new ArrayList<Integer>();
+						tmp.add(nums[i]);
+						tmp.add(nums[left]);
+						tmp.add(nums[right]);
+						result.add(tmp);
+						left++;
+						right--;
+						while (left < right && nums[left] == nums[left - 1]) { // to skip duplicates
+							left++;
+						}
+						while (left < right && nums[right] == nums[right + 1]) { // to skip duplicates
+							right--;
+						}
+	    			}
+	    		    else if(sum<0) {left++;}
+	    			else {	right--;}
+	    		}
+	    	}
+	        return result;
+	    }
     
     public static void main(String[] args){
     //	System.out.println(indexOfFirstNoBiggerThanKey(new int[] {1,2,3,4,5,6},0));
