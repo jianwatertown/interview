f4910e2ea69f0b6a0afe6c928e2fe4f3056b09dd
Jian Wang <jian.wang>
2016-10-21 23:27:01 -0700

    word ladder

--- interviewPractice/src/dynamicProgramming/WordLadder.java
+++ interviewPractice/src/dynamicProgramming/WordLadder.java
@@ -4,88 +4,56 @@ import java.util.*;
 public class WordLadder {
 	
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
-
-    	/**
-    	 * 1. 	Words are organized Graph, so this is Graph traversal problem
-    	 * 2.	Graph needs a "visited" set (graph traversal always needs this)
-    	 * 
-    	 * 		DFS approach does not work. Because different travel preferences give you different visiting paths,
-    	 * 		DFS generates local optimal, not necessarily global optimal.
-    	 * 
-    	 *		Algorithm: BFS by level order
-    	 *
-    	 *		1. Iterative: left Queue, right Queue
-    	 *  	2. Recursion
-    	 *  
-    	 *  	Undirected graph
-    	 *  	1. Set "visited" has node seen so far
-    	 *  
-    	 *  	Utility:
-    	 *  	1.	function to find "friends"
-    	 *  
-    	 */
-        return ladderLengthWithTwoQueues(endWord, wordList);
+    	Map<String,Integer> visited = new HashMap<>();
+    	Set<String> visiting = new HashSet<>();
+        return find(beginWord,endWord, wordList,visited,visiting);
     }
     
     
-    public int ladderLengthWithTwoQueues(String head, Set<String> dict){
+    public int find(String beginWord, String endWord, Set<String> wordList, Map<String,Integer> visited, Set<String> visiting) {
 
-    	// init
-    	Queue<String> leftQ = new PriorityQueue<String>();
-    	Queue<String> rightQ = new PriorityQueue<String>();
-    	Set<String> seen = new HashSet<String>();
-    	seen.add(head);
-    	leftQ.add(head);
-    	
-    	int level=0;
-    	
-    	// when either of them is empty
-    	while((!leftQ.isEmpty())|| (!rightQ.isEmpty())){
-    		
-    		// scan left queue
-    		while(!leftQ.isEmpty()){
-    			String current = leftQ.poll();
-    			if(current.equals(head)){
-    				return level;
-    			}
-    			else{
-    				seen.add(current);
-    				for(String child: findBuddies(current,dict,seen)){
-    					rightQ.add(child);
-    				}
+    	if(beginWord.equals(endWord)){
+    		return 0;
+    	}
+
+    	int length = Integer.MAX_VALUE;
+
+    	// 1. find all friends
+    	Set<String> friends = findFriends(beginWord,wordList);
+    	visiting.add(beginWord);
+
+    	for(String frd:friends){
+    		if(visiting.contains(frd)){
+    			continue;
+    		}
+    		// 2. visited
+    		if(visited.containsKey(frd)){
+    			if(visited.get(frd)!=-1)
+    				length = Math.min(length,visited.get(frd)+1);
+    		}
+    		// 3. unvisited friend
+    		else{
+
+    			int distance = find(frd, endWord, wordList, visited, visiting);
+    			if(distance!=-1){
+    				length = Math.min(distance+1,length);
     			}
     		}
-    		
-    		// going to switch level
-    		level++;
-    		
-    		// right is empty
-    		// scan right queue
-    		while(!rightQ.isEmpty()){
-    			String current = rightQ.poll();
-    			if(current.equals(head)){
-    				return level;
-    			}
-    			else{
-    				seen.add(current);
-    				for(String child: findBuddies(current,dict,seen)){
-    					leftQ.add(child);
-    				}
-    			}
-    		}
-    		
-    		// going to switch level
-    		level++;
+    	}
+    	// not found
+    	if(length==Integer.MAX_VALUE){
+    		length = -1;
     	}
     	
-    	// made out of the loop, then it's a dead loop
-    	return -1;
+    	visiting.remove(beginWord);
+    	visited.put(beginWord,length);
+    	return length;
     }
 
     // Exclude from this set
-    public List<String> findBuddies(String myself, Set<String> wordList, Set<String> excludeSet){
+    public Set<String> findFriends(String myself, Set<String> wordList){
     	
-        List<String> result = new ArrayList<String>();
+    	Set<String> result = new HashSet<String>();
         
         // for every character in myself
         for(int i=0;i<myself.length();i++){
@@ -99,7 +67,6 @@ public class WordLadder {
                 
                 // if new node and valid
                 if(!myself.equals(b.toString()) 				// not itself
-                		&& !excludeSet.contains(b.toString())	// not exclude
                 		&& wordList.contains(b.toString())){	// valid
                     result.add(b.toString());
                 }
@@ -110,6 +77,7 @@ public class WordLadder {
     
     public static void main(String[] args){
     	WordLadder w = new WordLadder();
-    	System.out.println(w.ladderLength("hit","hot",  new HashSet<String>(Arrays.asList("hit", "hot","dot","dog","cog"))));
+//    	System.out.println(w.ladderLength("hit","hot",  new HashSet<String>(Arrays.asList("hit", "hot","dot","dog","cog"))));
+       	System.out.println(w.ladderLength("hit","hot",  new HashSet<String>(Arrays.asList("hit", "hot"))));
     }
 }
