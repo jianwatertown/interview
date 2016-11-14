11e9ed491e6488c35b1b91c6b8df14a65e382314
Jian Wang <jian.wang@redfin.com>
2016-11-14 11:46:05 -0800

    partition sum

---
+++ jianPractice/src/dynamicProgramming/PartitionEqualSubsetSum.java
@@ -0,0 +1,29 @@
+package dynamicProgramming;
+
+
+/**
+ * Given a non-empty array containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+ * 
+ * 	Each of the array element will not exceed 100.
+	The array size will not exceed 200.
+	
+	Input: [1, 5, 11, 5]
+
+    Output: true
+    
+    Explanation: The array can be partitioned as [1, 5, 5] and [11].
+    
+    Input: [1, 2, 3, 5]
+
+    Output: false
+    
+    Explanation: The array cannot be partitioned into equal sum subsets.
+    
+    
+    
+ * @author jian.wang
+ *
+ */
+public class PartitionEqualSubsetSum {
+	
+}
