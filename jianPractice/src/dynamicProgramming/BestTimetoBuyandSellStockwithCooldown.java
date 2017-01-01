532e4181266786811eba785dc31d98b94b085055
Jian Wang <james.wang.cs@gmail.com>
2017-01-01 11:11:40 -0800

    with cool down

--- jianPractice/src/dynamicProgramming/BestTimetoBuyandSellStockwithCooldown.java
+++ jianPractice/src/dynamicProgramming/BestTimetoBuyandSellStockwithCooldown.java
@@ -19,48 +19,45 @@ package dynamicProgramming;
  https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
 
 
+ Answer:
+
  Key is to model stage i, then find out stage (i+1)
 
+ // 0.
+ buy[i] means before day i what is the maxProfit for *any* sequence end with buy.
+
+ sell[i] means before day i what is the maxProfit for *any* sequence end with sell.
+
+ rest[i] means before day i what is the maxProfit for *any* sequence end with rest.
+
+ // 1.
+buy[i] = max(buy[i-1] /do nothing/, rest[i-1] - price /buy now/);
+sell[i] = max(sell[i-1] /do nothing/, buy[i-1] + price /sell now/) -> made sure sell after buy
+rest[i] = max(sell[i-1],buy[i-1],rest[i-1])
+
+ // 2. because rest[i]>=buy[i]
+ rest[i] = max(sell[i-1],buy[i-1],rest[i-1]) = max(sell[i-1],rest[i-1])
+ so rest was never after "buy", therefore, we don't need to worry about the state before
+ buy[i] = max(buy[i-1] /do nothing/, rest[i-1] - price /buy now/);
+
+ // 3. rest[i] < sell[i] ->  max(sell[i-1],rest[i-1]) = sell[i-1]
+ buy[i] = max(buy[i-1] /do nothing/, rest[i-1] - price /buy now/);
+ sell[i] = max(sell[i-1] /do nothing/, buy[i-1] + price /sell now/)
+ rest[i] = sell[i-1]
+
+ // 4. remove rest
+ buy[i] = max(buy[i-1], sell[i-2] - price)
+ sell[i] = max(sell[i-1],buy[i-1])
+
+ // 5. then move on
 
  */
 public class BestTimetoBuyandSellStockwithCooldown {
 
     // TODO the followoing code is incomplete
-    // TODO ust read this again https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
-    // TODO key point "One tricky point is how do you make sure you sell before you buy,
-    // TODO              since from the equations it seems that [buy, rest, buy] is entirely possible.
-    // TODO     Well, the answer lies within the fact that buy[i] <= rest[i] which means rest[i] = max(sell[i-1], rest[i-1]).
-    // TODO     That made sure [buy, rest, buy] is never occurred. "
-    // TODO *if we have done our job correctly"
-    //
+    // TODO just read this again https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
     public int maxProfit(int[] prices) {
 
-        if(prices==null||prices.length<2){
-            return 0;
-        }
-
-        int[] buy = new int[prices.length];
-        int[] sell = new int[prices.length];
-        int[] rest = new int[prices.length];
-
-        buy[0] = -prices[0];
-
-        for(int i=1;i<prices.length;i++){
-            int price  = prices[i];
-
-            // TODO the following code does not work
-            // no gaureente we have bought before
-            // no gaureente we have not bought before
-
-//            buy[i]  = Math.max(rest[i-1]-price, buy[i-1]);
-//            sell[i] = Math.max((buy[i-1]+price, sell[i-1]);
-//            rest[i] = Math.max((sell[i-1], buy[i-1], rest[i-1]);
-
-        }
-        return Math.max(sell[prices.length-1],rest[prices.length-1]);
-    }
-
-    public int maxProfitEditorial(int[] prices) {
         int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
         for (int price : prices) {
             prev_buy = buy;
