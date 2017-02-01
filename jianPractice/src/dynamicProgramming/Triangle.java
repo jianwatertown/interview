676e3c29a964c25518c6eecfb7a250b72dc5f0ab
Jian Wang <james.wang.cs@gmail.com>
2017-01-31 21:32:55 -0800

    done triangle

--- jianPractice/src/dynamicProgramming/Triangle.java
+++ jianPractice/src/dynamicProgramming/Triangle.java
@@ -1,5 +1,6 @@
 package dynamicProgramming;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,10 +23,26 @@ public class Triangle {
         for(int row=triangle.size()-2;row>=0;row--){
 
             // each column
-            for(int i=0;i<row;i++){
+            for(int i=0;i<row+1;i++){
                 result[i] = Math.min(result[i],result[i+1]) + triangle.get(row).get(i);
             }
         }
         return result[0];
+    }
+
+    public static void main(String[] args){
+        Triangle t = new Triangle();
+
+        List<Integer> a = new ArrayList<>();
+        a.add(1);
+
+        List<Integer> b = new ArrayList<>();
+        b.add(2);
+        b.add(3);
+
+        List<List<Integer>> input = new ArrayList<List<Integer>>();
+        input.add(a);
+        input.add(b);
+        System.out.println(t.minimumTotal(input));
     }
 }
