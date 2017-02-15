f5b7c7999f2b6668af453cb73678eb1972573560
Jian Wang <james.wang.cs@gmail.com>
2017-02-15 08:12:34 -0800

    mht wip

--- jianPractice/src/bfs/MinimumHeightTrees.java
+++ jianPractice/src/bfs/MinimumHeightTrees.java
@@ -1,13 +1,108 @@
 package bfs;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by jianwang on 2/14/17.
+ *
+ * Cannot mess up multiple BFS together!!!
+ *
+ *
+ *
  */
 public class MinimumHeightTrees {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        return null;
+
+        List<Integer> result = new LinkedList<>();
+        Map<Integer,Set<Integer>> map = new HashMap<>();
+        Map<Integer,Integer> distMap = new HashMap<>();
+        Set<Integer> leaves = new HashSet<>();
+
+        // 1. build edge map
+        for(int[] edge:edges){
+            addEdgeToMap(map,edge[0],edge[1],leaves);
+            addEdgeToMap(map,edge[1],edge[0],leaves);
+        }
+
+        // 2. set initial distances
+        for(int i=0;i<n;i++){
+            distMap.put(i,0);
+        }
+
+        // 3. bfs all 0 - distance leaves
+        int maxDistance = 0;
+        for(Integer key: leaves){
+            maxDistance = Math.max(maxDistance,bfs(distMap,map,key,leaves));
+        }
+
+        // 4. Find the roots
+        for(Integer key:distMap.keySet()){
+            if(distMap.get(key)==maxDistance){
+                result.add(key);
+            }
+        }
+        return result;
     }
 
+
+    // bfs on per node basis
+    public int bfs(Map<Integer,Integer> distMap, Map<Integer,Set<Integer>>map, int start, Set<Integer> leaves){
+
+        int maxDistance = 0;
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(start,0));
+
+        while(!q.isEmpty()){
+            Point p = q.poll();
+            // add all friends
+            for(Integer c:map.get(p.val)){
+                if(!leaves.contains(p.val)) {
+                    Point childNode = new Point(c,p.distance+1);
+                    q.add(childNode);
+                    // update global max if needed
+                    distMap.put(distMap.get(c),childNode.distance);
+
+                    maxDistance = Math.max(maxDistance,distMap.get(c));
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+
+    public void addEdgeToMap(Map<Integer,Set<Integer>>map, int key, int value, Set<Integer> leaves){
+        // add if missing
+        if(!map.containsKey(key)){
+            map.put(key, new HashSet<>());
+        }
+        map.get(key).add(value);
+
+        // fix the size of leaf set
+        if(map.get(key).size()==1){
+            leaves.add(key);
+        }
+        else{
+            if(leaves.contains(key)){
+                leaves.remove(key);
+            }
+        }
+    }
+
+
+    public static class Point{
+        int val;
+        int distance;
+        public Point(int v, int distance){
+            this.val = v;
+            this.distance = distance;
+        }
+    }
+
+    public static void main(String[] args){
+        MinimumHeightTrees tester = new MinimumHeightTrees();
+        int[][] input = new int [][]{
+            {3,0},{3,1},{3,2},{3,4},{5,4},
+        };
+        System.out.println(tester.findMinHeightTrees(6,input));
+    }
 }
