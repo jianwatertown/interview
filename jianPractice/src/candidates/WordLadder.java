0e0d1bd19d2402f06181d1d58ec59d9caafec598
Jian Wang <jian.wang@redfin.com>
2017-02-21 09:45:08 -0800

    world ladder

--- jianPractice/src/candidates/WordLadder.java
+++ jianPractice/src/candidates/WordLadder.java
@@ -6,11 +6,13 @@ import java.util.*;
 /***
  *
  *
- * Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest
- * transformation sequen  ce from beginWord to endWord, such that:
+ * Given two words (beginWord and endWord), and a dictionary's word list,
+ * find the length of shortest
+ * transformation sequence from beginWord to endWord, such that:
 
 	 Only one letter can be changed at a time.
-	 Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+	 Each transformed word must exist in the word list. Note that beginWord is not a
+ transformed word.
 
 
  For example,
