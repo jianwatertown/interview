8232cd3d5e6115663875c4ef14159c64817dbdd9
Jian Wang <james.wang.cs@gmail.com>
2017-03-06 07:28:31 -0800

    sorted aray to bst

--- jianPractice/src/dfs/ConvertSortedArrayToBinarySearchTree.java
+++ jianPractice/src/dfs/ConvertSortedArrayToBinarySearchTree.java
@@ -10,15 +10,14 @@ public class ConvertSortedArrayToBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
         if (nums == null || nums.length == 0) return null;
         return getNode(nums,0,nums.length-1);
-5
     }
 
     public TreeNode getNode(int[] nums, int start, int end){
-        if(end>start) {return null;};
+        if(end<start) {return null;};
         int mid = (start+end)/2;
+        TreeNode root = new TreeNode(nums[mid]);
         TreeNode left = getNode(nums,start,mid-1);
         TreeNode right = getNode(nums,mid+1,end);
-        TreeNode root = new TreeNode(nums[mid]);
         root.left = left;
         root.right = right;
         return root;
