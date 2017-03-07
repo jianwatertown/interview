3e597323c0a747df2d0277a60e94db033a61d170
Jian Wang <james.wang.cs@gmail.com>
2017-03-07 08:15:31 -0800

    max path sum

--- jianPractice/src/dfs/BinaryTreeMaximumPathSum.java
+++ jianPractice/src/dfs/BinaryTreeMaximumPathSum.java
@@ -6,8 +6,35 @@ import tree.TreeNode;
  * Created by jianwang on 3/6/17.
  */
 public class BinaryTreeMaximumPathSum {
+
     public int maxPathSum(TreeNode root) {
-        return 0;
+        return helper(root).max();
+    }
+
+    // with/without
+    public Sum helper(TreeNode root){
+        if(root==null) {return new Sum(Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE);}
+        Sum left = helper(root.left);
+        Sum right = helper(root.right);
+        int without = Math.max(left.max(),right.max());
+        int withOnePath = Math.max(left.withOnePath,right.withOnePath) + root.val;
+        int withAsRoot = left.withOnePath + right.withOnePath + root.val;
+        return new Sum(without,withOnePath,withAsRoot);
+    }
+
+    public static class Sum{
+        int without;
+        int withAsRoot;
+        int withOnePath;
+        public Sum(int a, int b, int c){
+            this.without = a;
+            this.withAsRoot = b;
+            this.withOnePath = c;
+        }
+
+        public int max(){
+            return Math.max(Math.max(without,withAsRoot),withOnePath);
+        }
     }
 
 }
