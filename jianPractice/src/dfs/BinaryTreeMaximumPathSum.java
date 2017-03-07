e414dab61f68007b2becca9fd88751b9acf2e684
Jian Wang <james.wang.cs@gmail.com>
2017-03-06 22:36:02 -0800

    pre-order tree nailed

---
+++ jianPractice/src/dfs/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,13 @@
+package dfs;
+
+import tree.TreeNode;
+
+/**
+ * Created by jianwang on 3/6/17.
+ */
+public class BinaryTreeMaximumPathSum {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+
+}
--- jianPractice/src/tree/ConstructTreePreOrder.java
+++ jianPractice/src/tree/ConstructTreePreOrder.java
@@ -49,6 +49,29 @@ public class ConstructTreePreOrder {
     	}
     	return -1;
     }
+
+
+    // ------------------------------------------------------------------------------------------
+	//	use base case tree to figure out the indexes
+	// practise 03/06/2017
+	// ------------------------------------------------------------------------------------------
+	public TreeNode buildTree2(int[] preorder, int[] inorder) {
+		return null;
+	}
+
+	public TreeNode helper(int[] pre, int[] in, int pS, int pE, int iS, int iE){
+    	// 1. out bound then, null
+    	if(pS>pE||iS>iE||pS<0||pE>=pre.length||iS<0||iE>=in.length){return null;}
+		// 2. find root index
+		int rootIndex = findIndex(iS,pre[pS],in);
+    	TreeNode root = new TreeNode(pre[pS]);
+    	// 3. split the range
+    	root.left = helper(pre,in,pS+1,pS+(rootIndex-iS),iS,rootIndex-1);
+		root.right = helper(pre,in,pS+(rootIndex-iS)+1,pE,rootIndex+1,iE);
+    	return root;
+	}
+
+
     
     public static void main(String[] args){
 		// construct
