799dd150e4eb208f92c845f072c2a98c4d289397
Jian Wang <james.wang.cs@gmail.com>
2017-03-24 08:42:37 -0700

    next node

--- jianPractice/src/dfs/PopulatingNextRightPointersInEachNodeTwo.java
+++ jianPractice/src/dfs/PopulatingNextRightPointersInEachNodeTwo.java
@@ -3,16 +3,24 @@ package dfs;
 /**
  * Created by jianwang on 3/6/17.
  */
-public class PopulatingNextRightPointersInEachNode {
+public class PopulatingNextRightPointersInEachNodeTwo {
 
     public void connect(TreeLinkNode root) {
         if(root==null) {return;}
 
         // 1. connect immediate children first
+        if(root.right!=null){
+            
+        }
+
+
         if(root.left!=null){
-            root.left.next = root.right;
+
+            if(root.right)
+
+            root.left.next = root.right==null? findFirstLeft(root):right.n;
             // 2. connect right child to my sibling's left
-            if(root.next!=null){
+            if(root.next!=null && root.right!=null /*protected by root.left!=null already*/){
                 root.right.next = root.next.left;
             }
         }
@@ -20,6 +28,15 @@ public class PopulatingNextRightPointersInEachNode {
         // 2. move on
         connect(root.left);
         connect(root.right);
+    }
+
+    TreeLinkNode findFirstLeft(TreeLinkNode root){
+        while(root!=null){
+            if(root.left!=null) {return root.left;}
+            if(root.right!=null) {return root.right;}
+            root = root.right;
+        }
+        return null;
     }
 
     public class TreeLinkNode {
