da9842ffebf3fee28a5b3cf7dacd9818dfb6a389
Jian Wang <james.wang.cs@gmail.com>
2017-04-02 12:10:59 -0700

    watch boundary cases

--- jianPractice/src/dynamicProgramming/UniquePath2.java
+++ jianPractice/src/dynamicProgramming/UniquePath2.java
@@ -27,21 +27,22 @@ public class UniquePath2 {
         }
         int maxY = obstacleGrid[0].length;
         int maxX = obstacleGrid.length;
-        int[][] ways = new int[maxX][maxX];
+        int[][] ways = new int[maxX][maxY];
         for(int i=0;i<maxX;i++){
             Arrays.fill(ways[i],0);
         }
 
         // 2. from left upper corner to right bottom corner
+        if(!isIn(0,0,obstacleGrid)){return 0;}
         ways[0][0] = 1;
         for(int i=0;i<=maxX;i++){
             for(int j=0;j<=maxY;j++){
                 if(isIn(i,j,obstacleGrid)){
                     if(isIn(i+1,j,obstacleGrid)){
-                        ways[i+1][j] ++;
+                        ways[i+1][j] +=ways[i][j];
                     }
                     if(isIn(i,j+1,obstacleGrid)){
-                        ways[i][j+1] ++;
+                        ways[i][j+1] +=ways[i][j];
                     }
                 }
             }
