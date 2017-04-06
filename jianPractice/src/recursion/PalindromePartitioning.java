d3965390c9a5c3bdc8b182712754c672d6135cf6
Jian Wang <james.wang.cs@gmail.com>
2017-04-06 08:26:21 -0700

    wip

--- jianPractice/src/recursion/PalindromePartitioning.java
+++ jianPractice/src/recursion/PalindromePartitioning.java
@@ -1,10 +1,26 @@
 package recursion;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class PalindromePartitioning {
     public List<List<String>> partition(String s) {
+        List<List<String>> result = new LinkedList<>();
+
+
+        return result;
+    }
+
+    public void backtrack(List<List<String>> result, List<String> soFar, int){
 
     }
 
+
+    public boolean isPali(String str, int l, int h){
+        while(l<h){
+            if(str.charAt(l)!=str.charAt(h)){return false;}
+            l++;h++;
+        }
+        return true;
+    }
 }
