62d1f2d37403e26f8c9fa84f97a36ea34573f075
Jian Wang <james.wang.cs@gmail.com>
2017-04-07 08:08:13 -0700

    wip

--- jianPractice/src/recursion/Subsets.java
+++ jianPractice/src/recursion/Subsets.java
@@ -2,7 +2,6 @@ package recursion;
 
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
@@ -14,7 +13,6 @@ public class Subsets {
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(nums);
         backtrack(list, new ArrayList<>(), nums, 0);
         return list;
     }
