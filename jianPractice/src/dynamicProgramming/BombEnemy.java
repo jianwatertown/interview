b9907ba8ae8ca0d64dd4c618c8245a1956672572
Jian Wang <jian.wang@redfin.com>
2017-04-27 14:15:31 -0700

    bomb enemy

--- jianPractice/src/dynamicProgramming/BombEnemy.java
+++ jianPractice/src/dynamicProgramming/BombEnemy.java
@@ -24,147 +24,15 @@ package dynamicProgramming;
 public class BombEnemy {
 
 	public int maxKilledEnemies(char[][] grid) {
-		if(grid == null || grid.length == 0 ||  grid[0].length == 0) return 0;
-		int max = 0;
-		int row = 0;
-		int[] col = new int[grid[0].length];
-		for(int i = 0; i<grid.length; i++){
-			for(int j = 0; j<grid[0].length;j++){
-				if(grid[i][j] == 'W') continue;
-				if(j == 0 || grid[i][j-1] == 'W'){
-					row = killedEnemiesRow(grid, i, j);
-				}
-				if(i == 0 || grid[i-1][j] == 'W'){
-					col[j] = killedEnemiesCol(grid,i,j);
-				}
-				if(grid[i][j] == '0'){
-					max = (row + col[j] > max) ? row + col[j] : max;
-				}
-			}
 
-		}
-
-		return max;
-	}
-
-	//calculate killed enemies for row i from column j
-	private int killedEnemiesRow(char[][] grid, int i, int j){
-		int num = 0;
-		while(j <= grid[0].length-1 && grid[i][j] != 'W'){
-			if(grid[i][j] == 'E') num++;
-			j++;
-		}
-		return num;
-	}
-	//calculate killed enemies for  column j from row i
-	private int killedEnemiesCol(char[][] grid, int i, int j){
-		int num = 0;
-		while(i <= grid.length -1 && grid[i][j] != 'W'){
-			if(grid[i][j] == 'E') num++;
-			i++;
-		}
-		return num;
-	}
-
-
-    public int maxKilledEnemies2(char[][] grid) {
-    	
     	if(grid.length==0 || grid[0].length==0){return 0;}
     	
     	// 0. 
     	int max = 0;
-    	int x = grid.length;		// easier to use x,y
-    	int y = grid[0].length;		//
-    	Point[][] cache = new Point[x][y];
-    	for(int i=0;i<x;i++){
-    		for(int j=0;j<y;j++){
-    			cache[i][j] = new Point();
-    		}
-    	}
-    	
-    	// 1. n^2 traversal from top left to bottom right. 
-    	// populate leftToHere[] and upToHere[]
-    	for(int i=0;i<x;i++){
-    		for(int j=0;j<y;j++){
-    			char c = grid[i][j];
-    			int left = i>0?cache[i-1][j].left:0;	// use left
-    			int up = j>0?cache[i][j-1].up:0;		// use up
-    			// 1.1 'W' -> nothing can be reached from here, keep 0
-    			// 1.2 '0' -> place a bomb
-    			if(c=='0'){
-        			// update current
-        			cache[i][j].left = left;
-        			cache[i][j].up = up; 
-    			}
-    			// 1.3  'E' -> cannot place a bomb, but can update all the values
-    			else if(c=='E'){
-        			// update current
-        			cache[i][j].left = left+1;
-        			cache[i][j].up = up+1;
-    			}
-    		}
-    	}
-    	
-    	
-    	// 2. n^2 traversal from bottom right to upper right. 
-    	// populate rightToHere[] and downToHere[]
-    	// also keep the global max here
-    	for(int i=x-1;i>=0;i--){
-    		for(int j=y-1;j>=0;j--){
-    			char c = grid[i][j];
-    			int right = i<x-1?cache[i+1][j].right:0;	// use right
-    			int down = j<y-1?cache[i][j+1].down:0;		// use down
-    			// 2.1 'W' -> nothing can be reached from here, keep 0
-    			// 2.2 '0' -> place a bomb
-    			if(c=='0'){
-        			// update current
-        			cache[i][j].right = right;
-        			cache[i][j].down = down;
-        			max = Math.max(max, cache[i][j].getSum());
-    			}
-    			// 2.3  'E' -> cannot place a bomb, but can update all the values
-    			else if(c=='E'){
-        			// update current
-        			cache[i][j].right = right+1;
-        			cache[i][j].down = down+1;
-    			}
-    		}
-    	}
-    	return max;
-    }
+    	int x = grid.length, y= grid[0].length;		// easier to use x,y
 
-    public static class Point{
-    	public int left=0;			// enemy to the left
-    	public int right=0;		// enemy to the right 
-    	public int up=0;			// enemy from top
-    	public int down=0;			// enemy from bottom
-		public int getLeft() {
-			return left;
-		}
-		public int getRight() {
-			return right;
-		}
-		public int getUp() {
-			return up;
-		}
-		public int getDown() {
-			return down;
-		}
-		
-		public int getSum(){
-			return left+right+up+down;
-		}
-    }
-    
-    public int maxKilledEnemiesArray(char[][] grid) {
-    	
-    	if(grid.length==0 || grid[0].length==0){return 0;}
-    	
-    	// 0. 
-    	int max = 0;
-    	int x = grid.length;		// easier to use x,y
-    	int y = grid[0].length;		//
-    	int[][][] cache = new int[x][y][4];		// left,up,right,down
+		// left,up,right,down
+    	int[][][] cache = new int[x][y][4];
     	for(int i=0;i<x;i++){
     		for(int j=0;j<y;j++){
     			cache[i][j] = new int[4];
